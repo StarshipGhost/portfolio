adc9ee2fcd613c249445d21535def024f272a370
Saad Kalyati <saad.kalyati.1@ens.etsmtl.ca>
2026-04-03 03:45:38 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Header from './components/sections/Header'
+import Hero from './components/sections/Hero'
+
+
+function App() {
+  return (
+    <div className='min-w-80'> 
+      <div className='fixed inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-size-[14px_24px] mask-[radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]'></div>
+      <div className='fixed top-0 left-1/4 size-96 bg-cyan-500/20 blur-3xl rounded-full animate-pulse'></div>
+      <div className='fixed bottom-0 right-1/4 size-96 bg-purple-500/20 blur-3xl rounded-full animate-pulse'></div>
+      <Header/> 
+      <Hero/>
+      
+    </div>
+  )
+}
+
+export default App
