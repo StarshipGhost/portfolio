8a37ff9f23d6c3fa17d63d506ab3831057b6cc8b
Saad Kalyati <saad.kalyati.1@ens.etsmtl.ca>
2026-04-04 17:39:22 -0400

    Add more project properties variable populate the section with its header and subheader

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -1,28 +1,43 @@
 import project_1 from '../../assets/hoops-league-registration-app.png';
+import project_2 from '../../assets/iris-predictor-web-app.png';
+import project_3 from '../../assets/dofus-book.png';
+import BracketWrapper from '../BracketWrapper';
 import ExternalLinkIcon from '../icons/ExternalLinkIcon';
 import GithubIcon from '../icons/GithubIcon';
 
 interface ProjectCardProp {
     name: string,
     description: string,
+    liveDemo: string,
+    repo: string,
     technologies: string[]
+    img: string
 }
 
 const projects : ProjectCardProp[] = [
         {
             name: 'Hoops-league-registration-app',
-            description: 'A full-stack basketball game registration app.It allows users to view upcoming games, register for the next available event, and lets an admin manage the schedule and player registrations.',
-            technologies: ['React', 'Typescript', 'Tailwind CSS', 'Node.js', 'Express.js', 'MongoDB']
+            description: 'A full-stack basketball game registration app. It allows users to view upcoming games, register for the next available event, and lets an admin manage the schedule and player registrations.',
+            liveDemo: "http://starshipghost.github.io/hoops-league-registration-app", 
+            repo: "http://github.com/starshipghost/hoops-league-registration-app/tree/main", 
+            technologies: ['React', 'Typescript', 'Tailwind CSS', 'Node.js', 'Express.js', 'MongoDB'],
+            img: project_1 
         },
         {
-            name: 'Hoops-league-registration-app',
-            description: 'A full-stack basketball game registration app.It allows users to view upcoming games, register for the next available event, and lets an admin manage the schedule and player registrations.',
-            technologies: ['React', 'Typescript', 'Tailwind CSS', 'Node.js', 'Express.js', 'MongoDB']
+            name: 'Iris predictor web application',
+            description: 'An introductory ML application centered on Iris flower classification. It showcases how to take a well-known dataset, train a model with Python tools such as scikit-learn, and wrap it in a simple app or script so users can test predictions interactively.',
+            liveDemo: "#", 
+            repo: "https://github.com/jalalk97/iris-classification-app/", 
+            technologies: ['React', 'Typescript', 'CSS', 'Python', 'scikit-learn', 'pandas', 'matplotlib', 'Fast API'],
+            img: project_2
         },
         {
-            name: 'Hoops-league-registration-app',
-            description: 'A full-stack basketball game registration app.It allows users to view upcoming games, register for the next available event, and lets an admin manage the schedule and player registrations.',
-            technologies: ['React', 'Typescript', 'Tailwind CSS', 'Node.js', 'Express.js', 'MongoDB']
+            name: 'Dofus book clone',
+            description: 'A React and TypeScript web application that recreates part of the DofusDB experience. It extracts item and recipe data from the DofusDB API, transforms the raw responses into structured models, and displays information such as effects, conditions, descriptions, and recipe details through a clear game-data interface.',
+            liveDemo: "#", 
+            repo: "https://github.com/StarshipGhost/dofus-book/", 
+            technologies: ['React', 'Typescript', 'DofusDB API'],
+            img: project_3
         },
 
 ] 
@@ -30,19 +45,19 @@ const TechTags = ({name} : {name: string}) => {
     return <span className='text-purple-400 text-xs font-bold px-2 py-1 border border-solid border-purple-900 rounded-lg'>{name}</span>
 }
 
-const ProjectCard = ({project : {name, description, technologies}} : {project: ProjectCardProp}) => {
+const ProjectCard = ({project : {name, description, liveDemo, repo, technologies, img}} : {project: ProjectCardProp}) => {
     return (
-        <div className='flex flex-col bg-black/40 backdrop-blur-3xl border border-solid border-purple-500/30 hover:border-purple-400 rounded-xl'>
-            <img src={project_1} className='h-50 bg-purple-500 rounded-tl-xl rounded-tr-xl'></img>
+        <div className='bg-black backdrop-blur-3xl border border-solid border-purple-500/30 hover:border-purple-400 rounded-xl'>
+            <img src={img} className='h-50 w-full border border-solid border-purple-500/30 hover:border-purple-400 rounded-tl-xl rounded-tr-xl'></img>
             <div className="flex flex-col gap-4 p-4">
                 <h2 className='text-white text-xl'>{name}</h2>
                 <p className='text-sm text-neutral-400'>{description}</p>
                 <div className='flex flex-wrap gap-2'>
                     {technologies.map(tech => <TechTags name={tech}/>)}
                 </div>
-                <div className='w-full bg-destructive grid grid-cols-1 md:grid-cols-2 gap-4'>
-                    <button className="flex justify-center items-center h-9 text-white text-sm font-medium bg-linear-to-r from-cyan-500 hover:from-cyan-600 via-background to-purple-500 hover:to-purple-600 border border-solid border-cyan-900 rounded-lg whitespace-nowrap cursor-pointer"><ExternalLinkIcon/> Live Code </button>
-                    <button className='text-purple-400 text-sm font-medium flex justify-center items-center gap-2 border border-solid border-purple-500/50 hover:border-purple-400 hover:bg-purple-500/10'><GithubIcon className=''/> Code </button>
+                <div className='w-full grid grid-cols-1 md:grid-cols-2 gap-4'>
+                    <a href={liveDemo} className="flex justify-center items-center h-9 text-white text-sm font-medium bg-linear-to-r from-cyan-500 hover:from-cyan-600 to-purple-500 hover:to-purple-600 border border-solid border-cyan-900 rounded-lg whitespace-nowrap cursor-pointer"><ExternalLinkIcon/> Live Demo </a>
+                    <a href={repo} className='text-purple-400 hover:text-primary-foreground text-sm font-medium h-9 flex justify-center items-center gap-2 border border-solid border-purple-500/50 hover:border-purple-400 bg-accent-foreground hover:bg-purple-500/10 rounded-lg cursor-pointer'><GithubIcon className=''/> Code </a>
                 </div>
             </div>
         </div>
@@ -50,18 +65,19 @@ const ProjectCard = ({project : {name, description, technologies}} : {project: P
 }
 const ProjectCards = ({projects} : {projects: ProjectCardProp[]}) => {
     return (
-        <div className="w-full grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-8 mt-6">
+        <div className="w-full grid grid-cols-1 lg:grid-cols-3 sm:grid-cols-2 gap-8">
             {projects.map(project => <ProjectCard project={project}/>)}
         </div>
-        
     )
 }
 
 const Projects = () => {
     return (
-        <div className="max-w-6xl mx-auto px-4 py-20">
+        <section className="max-w-7xl mx-auto flex flex-col gap-4 items-center px-4 py-20">
+            <BracketWrapper color={'text-purple-400'} className="text-3xl lg:text-4xl text-white font-bold">Featured Projects</BracketWrapper>
+            <p className="text-neutral-400 text-lg tracking-wide text-center mb-8">A selection of projects I've worked on recently</p>
             <ProjectCards projects={projects}/>
-        </div>
+        </section>
     )
 }
 
