c7ed201cc1bd11a1a934e2210257b5cb8e882ecd
Saad Kalyati <saad.kalyati.1@ens.etsmtl.ca>
2026-04-06 02:13:44 -0400

    Change project description, add project libraries and add a fixed height to project description

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -25,10 +25,10 @@ const projects : ProjectCardProp[] = [
         },
         {
             name: 'Iris predictor web application',
-            description: 'An introductory ML application centered on Iris flower classification. It showcases how to take a well-known dataset, train a model with Python tools such as scikit-learn, and wrap it in a simple app or script so users can test predictions interactively.',
+            description: 'An introductory ML application centered on Iris flower classification. It showcases how to take a well-known dataset, train a model with Python tools such as scikit-learn, and wrap it in a simple app so users can test predictions interactively.',
             liveDemo: "#", 
             repo: "https://github.com/jalalk97/iris-classification-app/", 
-            technologies: ['React', 'Typescript', 'CSS', 'Python', 'scikit-learn', 'pandas', 'matplotlib', 'Fast API'],
+            technologies: ['React', 'Typescript', 'Python', 'scikit-learn', 'pandas', 'matplotlib', 'Fast API'],
             img: project_2
         },
         {
@@ -36,12 +36,12 @@ const projects : ProjectCardProp[] = [
             description: 'A React and TypeScript web application that recreates part of the DofusDB experience. It extracts item and recipe data from the DofusDB API, transforms the raw responses into structured models, and displays information such as effects, conditions, descriptions, and recipe details through a clear game-data interface.',
             liveDemo: "#", 
             repo: "https://github.com/StarshipGhost/dofus-book/", 
-            technologies: ['React', 'Typescript', 'DofusDB API'],
+            technologies: ['React', 'Typescript', 'CSS', 'DofusDB API'],
             img: project_3
         },
 
 ] 
-const TechTags = ({name} : {name: string}) => {
+const TechTag = ({name} : {name: string}) => {
     return <span className='text-purple-400 text-xs font-bold px-2 py-1 border border-solid border-purple-900 rounded-lg'>{name}</span>
 }
 
@@ -51,13 +51,13 @@ const ProjectCard = ({project : {name, description, liveDemo, repo, technologies
             <img src={img} className='h-50 w-full border border-solid border-purple-500/30 hover:border-purple-400 rounded-tl-xl rounded-tr-xl'></img>
             <div className="flex flex-col gap-4 p-4">
                 <h2 className='text-white text-xl'>{name}</h2>
-                <p className='text-sm text-neutral-400'>{description}</p>
+                <p className='h-35 text-sm text-neutral-400 overflow-y-auto'>{description}</p>
                 <div className='flex flex-wrap gap-2'>
-                    {technologies.map(tech => <TechTags name={tech}/>)}
+                    {technologies.map(tech => <TechTag key={tech} name={tech}/>)}
                 </div>
                 <div className='w-full grid grid-cols-1 md:grid-cols-2 gap-4'>
                     <a href={liveDemo} className="flex justify-center items-center h-9 text-white text-sm font-medium bg-linear-to-r from-cyan-500 hover:from-cyan-600 to-purple-500 hover:to-purple-600 border border-solid border-cyan-900 rounded-lg whitespace-nowrap cursor-pointer"><ExternalLinkIcon/> Live Demo </a>
-                    <a href={repo} className='text-purple-400 hover:text-primary-foreground text-sm font-medium h-9 flex justify-center items-center gap-2 border border-solid border-purple-500/50 hover:border-purple-400 bg-accent-foreground hover:bg-purple-500/10 rounded-lg cursor-pointer'><GithubIcon className=''/> Code </a>
+                    <a href={repo} className='text-purple-400 hover:text-primary-foreground text-sm font-medium h-9 flex justify-center items-center gap-2 border border-solid border-purple-500/50 hover:border-purple-400 bg-accent-foreground hover:bg-purple-500/10 rounded-lg cursor-pointer'><GithubIcon /> Code </a>
                 </div>
             </div>
         </div>
@@ -66,7 +66,7 @@ const ProjectCard = ({project : {name, description, liveDemo, repo, technologies
 const ProjectCards = ({projects} : {projects: ProjectCardProp[]}) => {
     return (
         <div className="w-full grid grid-cols-1 lg:grid-cols-3 sm:grid-cols-2 gap-8">
-            {projects.map(project => <ProjectCard project={project}/>)}
+            {projects.map(project => <ProjectCard key={project.name} project={project}/>)}
         </div>
     )
 }
@@ -74,8 +74,8 @@ const ProjectCards = ({projects} : {projects: ProjectCardProp[]}) => {
 const Projects = () => {
     return (
         <section className="max-w-7xl mx-auto flex flex-col gap-4 items-center px-4 py-20">
-            <BracketWrapper color={'text-purple-400'} className="text-3xl lg:text-4xl text-white font-bold">Featured Projects</BracketWrapper>
-            <p className="text-neutral-400 text-lg tracking-wide text-center mb-8">A selection of projects I've worked on recently</p>
+            <BracketWrapper color={'text-purple-400'} className="text-3xl lg:text-4xl text-white text-center font-bold">Featured Projects</BracketWrapper>
+            <p className="max-w-xl text-neutral-400 text-md lg:text-lg text-center mb-8">A selection of projects I've worked on recently</p>
             <ProjectCards projects={projects}/>
         </section>
     )
