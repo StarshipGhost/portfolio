a2a9542ff4e6a6107e151f409edff9be3f6ff6eb
Saad Kalyati <saad.kalyati.1@ens.etsmtl.ca>
2026-04-06 02:11:38 -0400

    Nest the icon components inside the anchor tags and add background color to profile picture's container

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -2,14 +2,16 @@ import BracketWrapper from "../BracketWrapper";
 import GithubIcon from "../icons/GithubIcon";
 import LinkedinIcon from "../icons/LinkedinIcon";
 import MailIcon from "../icons/MailIcon";
-import profile_picture from '../../assets/Portrait of a young man.png'
+import profile_picture from '../../assets/linkedin_profile_picture.png'
 
 const ProfilePicture = () => {
   return (
     <div className='flex justify-center items-center rounded-full order-1 lg:order-2'>
-        <div className='size-64 sm:size-72 md:size-80 lg:size-120'>
-            <div className="w-full h-full rounded-full bg-black p-2">
-                <img src={profile_picture} className="w-full h-full rounded-full object-cover"></img>
+        <div className='relative size-64 sm:size-72 md:size-80 lg:size-120'>
+            <div className="absolute w-full h-full bg-linear-to-br from-cyan-500 via-purple-500 to-pink-500 rounded-full p-1">
+                <div className="w-full h-full rounded-full bg-black p-2">
+                    <img src={profile_picture} className="w-full h-full rounded-full object-cover"></img>
+                </div>
             </div>
         </div>
     </div>
@@ -20,15 +22,15 @@ const Headline = () => {
     return (
         <div className="flex flex-col items-center lg:items-start order-2 lg:order-1">
             <h1 className="text-5xl lg:text-6xl text-white mb-2 text-center lg:text-left"> Hi, I'm <span className="bg-linear-to-r from-cyan-500 to-purple-500 text-transparent bg-clip-text"> Saâd Kalyati</span></h1>
-            <BracketWrapper color="text-cyan-500" className="text-2xl lg:text-3xl text-gray-300 text-center font-mono font-medium mb-6">
+            <BracketWrapper color="text-cyan-500" className="text-2xl lg:text-3xl text-gray-300 text-center lg:text-left font-mono font-medium mb-6">
                 Software Engineering Student
             </BracketWrapper>
             <div className="flex flex-col gap-4">
             <p className="text-lg text-neutral-400 font-normal mb-2 text-center lg:text-left">Passionate about building elegant solutions to complex problems. I specialize in creating scalable web applications with modern technologies and best practices. </p>
                 <div className="flex justify-center lg:justify-start gap-4 mb-6">
-                    <GithubIcon className="bg-black border border-box border-cyan-900 hover:border-cyan-500 hover:bg-cyan-500/10 p-2 rounded-md cursor-pointer text-cyan-500 hover:text-white"/>
-                    <LinkedinIcon/>
-                    <MailIcon/>
+                    <a className="bg-black border border-box border-cyan-900 hover:border-cyan-500 hover:bg-cyan-500/10 p-2 rounded-md cursor-pointer text-cyan-500 hover:text-white"><GithubIcon/></a>
+                    <a className="bg-black border border-box border-cyan-900 hover:border-cyan-500 hover:bg-cyan-500/10 p-2 rounded-md cursor-pointer text-cyan-500 hover:text-white"><LinkedinIcon/></a>
+                    <a className="bg-black border border-box border-cyan-900 hover:border-cyan-500 hover:bg-cyan-500/10 p-2 rounded-md cursor-pointer text-cyan-500 hover:text-white"><MailIcon/></a>
                 </div>
                 <div className="flex flex-wrap justify-center lg:justify-start gap-4">
                     <button className="w-38 h-9 text-white text-sm font-medium bg-linear-to-r from-cyan-500 hover:from-cyan-600 to-purple-500 hover:to-purple-600 border border-solid border-cyan-900 rounded-lg whitespace-nowrap cursor-pointer">View My Work</button>
