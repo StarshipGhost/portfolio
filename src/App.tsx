2e493edc95dde162ce3852338f868525f09051c0
Saad Kalyati <saad.kalyati.1@ens.etsmtl.ca>
2026-04-06 02:24:59 -0400

    Add scroll into view functionality and link resume on the header button

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import { useRef } from 'react'
 import './App.css'
+import Contacts from './components/sections/Contacts'
 import Education from './components/sections/Education'
 import Header from './components/sections/Header'
 import Hero from './components/sections/Hero'
@@ -7,16 +9,29 @@ import TechStack from './components/sections/TechStack'
 
 
 function App() {
+
+  const sectionRef = useRef<HTMLElement | null>(null);
+  const scroll = (index: number) => {
+    const sectionNode = sectionRef.current;  
+    if (sectionNode) {
+      const section = sectionNode.querySelectorAll('main > section')[index];
+      section.scrollIntoView({behavior: 'smooth', block: 'start'})
+    }
+  }
+
   return (
     <div className='min-w-80 bg-black'> 
       <div className='fixed inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-size-[14px_24px] mask-[radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]'></div>
       <div className='fixed top-0 left-1/4 size-96 bg-cyan-500/20 blur-3xl rounded-full animate-pulse pointer-events-auto'></div>
       <div className='fixed bottom-0 right-1/4 size-96 bg-purple-500/20 blur-3xl rounded-full animate-pulse pointer-events-auto'></div>
-      <Header/> 
-      <Hero/>
-      <TechStack/>
-      <Projects/>
-      <Education/>
+      <Header scroll={scroll}/> 
+      <main ref={sectionRef}>
+        <Hero/>
+        <TechStack/>
+        <Projects/>
+        <Education/>
+        <Contacts/>
+      </main>
     </div>
   )
 }
--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -3,49 +3,65 @@ import BracketWrapper from "../BracketWrapper";
 import MenuIcon from "../icons/MenuIcon";
 import clsx from "clsx";
 
-const MobileHeader = ({open, closeMenu}: {open: boolean, closeMenu: () => void}) => {
+const MobileHeader = ({open, closeMenu, scroll}: {open: boolean, closeMenu: () => void, scroll: (index: number) => void}) => {
     const baseClass : string = "lg:hidden w-full absolute -z-1 bg-black flex flex-col p-4 transition -translate-y-full duration-300 delay-200"
     const activeClass : string = open ? 'translate-y-0' : '-translate-y-full'
     const itemClass : string = "text-sm text-neutral-400 font-medium hover:bg-cyan-900/30 p-3 rounded-md cursor-pointer";
+    const navigate = (index: number) => {
+        scroll(index);
+        closeMenu();
+    }
     return (
         <ul className={clsx(baseClass, activeClass)}>
-            <li className={clsx(itemClass)} onClick={closeMenu}><a>Home</a></li>
-            <li className={clsx(itemClass)}onClick={closeMenu}><a>Tech Stack</a></li>
-            <li className={clsx(itemClass)}onClick={closeMenu}><a>Projects</a></li>
-            <li className={clsx(itemClass)}onClick={closeMenu}><a>Contacts</a></li>
+            <li className={clsx(itemClass)} onClick={() => navigate(0)}><a>Home</a></li>
+            <li className={clsx(itemClass)}onClick={() => navigate(1)}><a>Tech Stack</a></li>
+            <li className={clsx(itemClass)}onClick={() => navigate(2)}><a>Projects</a></li>
+            <li className={clsx(itemClass)}onClick={() => navigate(4)}><a>Contacts</a></li>
             <li className='text-white text-sm text-center font-medium bg-linear-to-r from-cyan-500 hover:from-cyan-600 to-purple-500 hover:to-purple-600 rounded-md p-2 cursor-pointer' onClick={closeMenu}><a>Resume</a></li>
         </ul>
     )
 }
 
-const DesktopHeader = () => {
+const DesktopHeader = ({scroll} : { scroll : (index: number) => void}) => {
     const itemClass : string = "text-sm text-neutral-400 font-medium hover:text-cyan-500 p-3 rounded-md cursor-pointer";
     return (
         <ul className="lg:flex hidden gap-2">
-            <li className={clsx(itemClass)}>Home</li>
-            <li className={clsx(itemClass)}>Tech Stack</li>
-            <li className={clsx(itemClass)}>Projects</li>
-            <li className={clsx(itemClass)}>Contacts</li>
+            <li className={clsx(itemClass)} onClick={() => scroll(0)}><a>Home</a></li>
+            <li className={clsx(itemClass)} onClick={() => scroll(1)} ><a>Tech Stack</a></li>
+            <li className={clsx(itemClass)} onClick={() => scroll(2)}><a>Projects</a></li>
+            <li className={clsx(itemClass)} onClick={() => scroll(4)}><a>Contacts</a></li>
         </ul>
     )
 }
 
-const Header = () => {
+const Header = ({scroll} : { scroll : (index: number) => void}) => {
     const [openMenu, setOpenMenu] = useState<boolean>(false);
+    const [hovered, setHovered] = useState<boolean>(false);
     const closeMenu = () => {
         setOpenMenu(false);
     }
+    const toggleHover = () => {
+        setHovered(!hovered);
+    }
+    const popoverBaseClass = 'absolute w-35 bg-black opacity-0 transition duration-200 pointer-event-none cursor-default'
+    const popoverActiveClass = hovered && 'opacity-100 pointer-event-auto cursor-pointer'
     return (
         <header className="sticky top-0 z-1">
             <div className="flex justify-between items-center bg-black lg:px-[5vw] px-4 py-2 border-b border-solid border-cyan-900/70">
                 <BracketWrapper color='' className="text-xl lg:text-2xl font-bold bg-linear-to-r from-cyan-500 to-purple-500 text-transparent bg-clip-text cursor-auto">
                     {`Portfolio `}
                 </BracketWrapper>
-                <DesktopHeader/>
+                <DesktopHeader scroll={scroll}/>
                 <MenuIcon open={openMenu} toggleMenu={() => setOpenMenu((v) => !v)}/>
-                <button className={'hidden lg:block text-white text-sm font-medium bg-linear-to-r from-cyan-500 hover:from-cyan-600 to-purple-500 hover:to-purple-600 px-4 py-1.5 rounded-md cursor-pointer'}>Resume</button>
+                <div className="hidden lg:block">
+                    <button onMouseEnter={() => setHovered(true)} onMouseLeave={toggleHover} className={' hidden relative lg:flex gap-2 items-center text-white text-sm font-medium bg-linear-to-r from-cyan-500 hover:from-cyan-600 to-purple-500 hover:to-purple-600 rounded-sm px-4 py-1.5 cursor-pointer'}> Resume <span className={ `size-0 border-l-5 border-r-5 border-t-5 border-solid border-l-transparent border-r-transparent border-t-white transition-transform duration-300 ${hovered && 'rotate-180'}` }></span></button>
+                    <ul className={ clsx(popoverBaseClass, popoverActiveClass) }>
+                        <li onMouseEnter={() => hovered && setHovered(true)} onMouseLeave={() => setHovered(false)} className={ `text-cyan-500 text-sm font-medium border border-box border-cyan-900 hover:border-cyan-500 hover:bg-cyan-500/10 ${hovered ? 'pointer-events-auto cursor-pointer' : 'pointer-events-none cursor-default'} p-4`}><a href="../../public/CV_Saâd_Kalyati_FR.pdf" target="_blank">French</a></li>
+                        <li onMouseEnter={() => hovered && setHovered(true)} onMouseLeave={() => setHovered(false)} className={ `text-cyan-500 text-sm font-medium border border-box border-cyan-900 hover:border-cyan-500 hover:bg-cyan-500/10 ${hovered ? 'pointer-events-auto cursor-pointer' : 'pointer-events-none cursor-default'} p-4` }><a href="../../public/CV_Saâd_Kalyati_EN.pdf" target="_blank">English</a></li>
+                    </ul>
+                </div>
             </div>
-            <MobileHeader open={openMenu} closeMenu={closeMenu}/>
+            <MobileHeader open={openMenu} closeMenu={closeMenu} scroll={scroll}/>
         </header>
     ) 
 }
