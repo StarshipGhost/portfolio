561deb4597c6e61fcd50d7214dde0e1f5fe6da9f
Saad Kalyati <saad.kalyati.1@ens.etsmtl.ca>
2026-04-06 02:08:28 -0400

    Add reponsive text size and change components dispositions based on card container's width

--- src/components/sections/Education.tsx
+++ src/components/sections/Education.tsx
@@ -17,7 +17,7 @@ const education : EducationCardProps[] = [
             icon: ets,
             school: 'École de Technologie Supérieure',
             program: 'Bachelor in Software Engineering',
-            specialization: 'Specialized in Software Architecture and Web Development',
+            specialization: 'Specialized in Software Architecture and Web Development.',
             year: '2023 - present'
         },
         {
@@ -32,12 +32,12 @@ const education : EducationCardProps[] = [
 const EducationCard = ({education : {icon, school, program, specialization, year}} : {education: EducationCardProps}) => {
     return (
         <div className="bg-cyan-500/10 backdrop-blur-3xl border border-solid border-cyan-900 hover:border-cyan-500 flex flex-col gap-4 rounded-lg p-4">
-            <div className='flex justify-between'>
+            <div className='flex flex-col gap-4 md:flex-row md:justify-between'>
                 <div className='flex gap-4'>
                     <img src={icon} className='size-11 rounded-lg'></img> 
                     <div className='space-y-2'>
                         <h2 className='text-white'>{school}</h2>
-                        <p className='text-cyan-500 font-medium'>{program}</p> 
+                        <p className='text-cyan-500 text-sm font-medium'>{program}</p> 
                     </div>
                 </div> 
                 <div className='text-neutral-400 text-sm tracking-tight flex gap-2 items-center'>
@@ -45,7 +45,7 @@ const EducationCard = ({education : {icon, school, program, specialization, year
                     <span>{year}</span>
                 </div>
             </div>
-            <p className='max-w-4xl text-neutral-400'>{specialization}</p>
+            <p className='max-w-4xl text-sm lg:text-normal text-neutral-400'>{specialization}</p>
         </div>
     )
 }
@@ -53,7 +53,7 @@ const EducationCard = ({education : {icon, school, program, specialization, year
 const EducationCards = ({education} : {education: EducationCardProps[]}) => {
     return (
         <div className='w-full space-y-8'>
-            {education.map(edu => <EducationCard education={edu}/>)};
+            {education.map(edu => <EducationCard key={edu.school} education={edu}/>)};
         </div>
     ) 
 }
@@ -62,8 +62,8 @@ const Education = () => {
     return (
         <section className='max-w-7xl mx-auto flex flex-col gap-4 items-center px-4 py-20'>
             <GraduationCapIcon/>
-            <BracketWrapper color={'text-cyan-500'} className="text-3xl lg:text-4xl text-white font-bold">Education</BracketWrapper>
-            <p className="text-neutral-400 text-md lg:text-lg tracking-wide text-center mb-8">My academic journey and qualifications</p>
+            <BracketWrapper color={'text-cyan-500'} className="text-3xl lg:text-4xl text-white text-center font-bold">Education</BracketWrapper>
+            <p className="max-w-xl text-neutral-400 text-md lg:text-lg tracking-wide text-center mb-8">My academic journey and qualifications</p>
             <EducationCards education={education}/>
         </section>
     )
