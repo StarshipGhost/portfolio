ef69efd144e16cf413e287c671f9425bb03fcf07
Saad Kalyati <saad.kalyati.1@ens.etsmtl.ca>
2026-04-06 02:04:04 -0400

    Keep icon from shrinking when the container is width is reduced

--- src/components/icons/CalendarIcon.tsx
+++ src/components/icons/CalendarIcon.tsx
@@ -1,6 +1,6 @@
 const CalendarIcon = () => {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-calendar w-4 h-4" aria-hidden="true" data-darkreader-inline-stroke="">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-calendar size-4 flex-none" aria-hidden="true" data-darkreader-inline-stroke="">
             <path d="M8 2v4"></path>
             <path d="M16 2v4"></path>
             <rect width="18" height="18" x="3" y="4" rx="2"></rect>
