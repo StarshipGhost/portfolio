ecd7f6aff378536b9c8afe10b4e89d8cf7753731
Saad Kalyati <saad.kalyati.1@ens.etsmtl.ca>
2026-04-10 02:59:20 -0400

    Add target property to display the links content in a new tab

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -28,41 +28,42 @@ const projects : ProjectCardProp[] = [
             description: 'An introductory ML application centered on Iris flower classification. It showcases how to take a well-known dataset, train a model with Python tools such as scikit-learn, and wrap it in a simple app so users can test predictions interactively.',
             liveDemo: "#", 
             repo: "https://github.com/jalalk97/iris-classification-app/", 
-            technologies: ['React', 'Typescript', 'Python', 'scikit-learn', 'pandas', 'matplotlib', 'Fast API'],
+            technologies: ['React', 'Typescript', 'Python', 'Fast API', 'scikit-learn', 'pandas', 'matplotlib'],
             img: project_2
         },
         {
             name: 'Dofus book clone',
             description: 'A React and TypeScript web application that recreates part of the DofusDB experience. It extracts item and recipe data from the DofusDB API, transforms the raw responses into structured models, and displays information such as effects, conditions, descriptions, and recipe details through a clear game-data interface.',
-            liveDemo: "#", 
+            liveDemo: "https://starshipghost.github.io/dofus-book/", 
             repo: "https://github.com/StarshipGhost/dofus-book/", 
-            technologies: ['React', 'Typescript', 'CSS', 'DofusDB API'],
+            technologies: ['React', 'Typescript', 'CSS'],
             img: project_3
         },
-
 ] 
+
 const TechTag = ({name} : {name: string}) => {
     return <span className='text-purple-400 text-xs font-bold px-2 py-1 border border-solid border-purple-900 rounded-lg'>{name}</span>
 }
 
 const ProjectCard = ({project : {name, description, liveDemo, repo, technologies, img}} : {project: ProjectCardProp}) => {
     return (
-        <div className='bg-black backdrop-blur-3xl border border-solid border-purple-500/30 hover:border-purple-400 rounded-xl'>
-            <img src={img} className='h-50 w-full border border-solid border-purple-500/30 hover:border-purple-400 rounded-tl-xl rounded-tr-xl'></img>
+        <div className='flex flex-col justify-between bg-black backdrop-blur-3xl border border-solid border-purple-500/30 hover:border-purple-400 rounded-xl'>
+            <img src={img} className='w-full border border-solid border-purple-500/30 hover:border-purple-400 rounded-tl-xl rounded-tr-xl'></img>
             <div className="flex flex-col gap-4 p-4">
                 <h2 className='text-white text-xl'>{name}</h2>
-                <p className='h-35 text-sm text-neutral-400 overflow-y-auto'>{description}</p>
+                <p className=' text-sm text-neutral-400 overflow-y-auto'>{description}</p>
                 <div className='flex flex-wrap gap-2'>
                     {technologies.map(tech => <TechTag key={tech} name={tech}/>)}
                 </div>
                 <div className='w-full grid grid-cols-1 md:grid-cols-2 gap-4'>
-                    <a href={liveDemo} className="flex justify-center items-center h-9 text-white text-sm font-medium bg-linear-to-r from-cyan-500 hover:from-cyan-600 to-purple-500 hover:to-purple-600 border border-solid border-cyan-900 rounded-lg whitespace-nowrap cursor-pointer"><ExternalLinkIcon/> Live Demo </a>
-                    <a href={repo} className='text-purple-400 hover:text-primary-foreground text-sm font-medium h-9 flex justify-center items-center gap-2 border border-solid border-purple-500/50 hover:border-purple-400 bg-accent-foreground hover:bg-purple-500/10 rounded-lg cursor-pointer'><GithubIcon /> Code </a>
+                    <a href={liveDemo} target="_blank" className="flex justify-center items-center h-9 text-white text-sm font-medium bg-linear-to-r from-cyan-500 hover:from-cyan-600 to-purple-500 hover:to-purple-600 border border-solid border-cyan-900 rounded-lg whitespace-nowrap cursor-pointer"><ExternalLinkIcon/> Live Demo </a>
+                    <a href={repo} target="_blank" className='text-purple-400 hover:text-primary-foreground text-sm font-medium h-9 flex justify-center items-center gap-2 border border-solid border-purple-500/50 hover:border-purple-400 bg-accent-foreground hover:bg-purple-500/10 rounded-lg cursor-pointer'><GithubIcon /> Code </a>
                 </div>
             </div>
         </div>
     )
 }
+
 const ProjectCards = ({projects} : {projects: ProjectCardProp[]}) => {
     return (
         <div className="w-full grid grid-cols-1 lg:grid-cols-3 sm:grid-cols-2 gap-8">
