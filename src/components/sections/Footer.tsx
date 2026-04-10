81a38fda59177e22cfd435935223f32d18dbff5d
Saad Kalyati <saad.kalyati.1@ens.etsmtl.ca>
2026-04-10 03:13:42 -0400

    Add and style website footer

---
+++ src/components/sections/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer =  () => {
+    return (
+        <footer className="h-20 flex flex-col justify-center bg-black border-t border-solid border-cyan-900/70">
+            <div className="text-sm lg:text-md text-white text-center">Copyright © 2026 - All right reserved by Saâd Kalyati</div>
+        </footer>
+    )
+}
+
+export default Footer
